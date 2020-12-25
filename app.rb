223be8fa3104643536e3d5aa0807feadbf2376a2
Michael Fuget <michaelfuget@gmail.com>
2020-12-25 05:32:20 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,10 +2,10 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   
-  configure do 
-    enable :sessions 
-    set :session_secret, "secret"
-  end
+configure do
+  enable :sessions
+  set :session_secret, "secret"
+end
   
   get '/' do 
     @session = session
